59ee7ed89ca719bf35bdf69ca2f5f0ac2e204107
Maxmudbek <muzaffarovmahmudbek@gmail.com>
2024-11-24 20:14:03 +0500

    cors

--- src/pages/api/swagger.ts
+++ src/pages/api/swagger.ts
@@ -1,15 +1,42 @@
+// pages/api/swagger.ts
 import { NextApiRequest, NextApiResponse } from 'next';
-import swaggerJsdoc, { Options } from 'swagger-jsdoc';
+import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerDefinition from '../../config/swagger';
+import cors from 'cors';
 
-const options: Options = {
+// CORS middleware
+const corsMiddleware = cors({
+  methods: ['GET', 'HEAD'],
+});
+
+const options = {
   definition: swaggerDefinition,
-  apis: ['./src/pages/api/**/*.ts'], // Ichki papkalarni ham qidiradi
-
+  apis: ['./src/pages/api/**/*.ts'], // API route fayllarining yo'li
 };
 
 const swaggerSpec = swaggerJsdoc(options);
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(swaggerSpec);
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // CORS ni handle qilish
+  await new Promise((resolve, reject) => {
+    corsMiddleware(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+
+  if (req.method === 'GET') {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(swaggerSpec);
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
