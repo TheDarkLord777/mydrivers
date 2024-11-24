fc936c961f70e162def706aeb1beff68eb1dedb9
Maxmudbek <muzaffarovmahmudbek@gmail.com>
2024-11-24 19:55:04 +0500

    check

--- src/pages/swagger.tsx
+++ src/pages/swagger.tsx
@@ -2,7 +2,8 @@ import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
 const SwaggerPage: React.FC = () => {
-  return <SwaggerUI url="/api/swagger" />;
+  return <SwaggerUI url={`${process.env.BASE_URL}/api/swagger`} />
+
 };
 
 export default SwaggerPage;
