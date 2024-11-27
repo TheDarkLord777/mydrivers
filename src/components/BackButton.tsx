81dafa33fb16cbe25d2e4f4e551cedb5f8701888
Maxmudbek <muzaffarovmahmudbek@gmail.com>
2024-11-27 23:27:32 +0500

    changes

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -12,10 +12,10 @@ const BackButton: React.FC = () => {
   return (
     <button
       onClick={handleClick}
-      className="flex items-center bg-red-500 text-white px-4 py-2 rounded relative group overflow-hidden transition-all duration-300"
+      className="flex items-center bg-red-500 text-white px-4 py-2 rounded relative group overflow-hidden transition-all duration-600"
     >
       <FiLogOut className="mr-2" />
-      <span className="ml-2 whitespace-nowrap overflow-hidden max-w-0 group-hover:max-w-xs transition-all duration-300">
+      <span className="ml-2 whitespace-nowrap overflow-hidden max-w-0 group-hover:max-w-xs transition-all duration-600">
         Orqaga
       </span>
     </button>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,37 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import ClickEffect from '@/components/ClickEffect';
+import ClickEffect from "@/components/ClickEffect";
+import { useRouter } from "next/router";
+import { useAuth } from "@/hooks/useAuth";
+import { Car } from 'lucide-react'
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-  <ClickEffect />
-  <Component {...pageProps} />
-</>;
+  const [loading, setLoading] = useState(true);
+  const user = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log("Authenticated user:", user);
+    if (user.user === null && !["/", "/login", "/register"].includes(router.pathname)) {
+      setLoading(true)
+    } else {
+      setLoading(false);
+    }
+  }, [user, router.pathname]);
+
+  if (loading) {
+    return <div className="flex items-center justify-center h-screen bg-blue-900">
+    <Car className="w-20 h-20 text-white animate-pulse" />
+  </div>;
+  }
+
+  return (
+    <>
+      <ClickEffect />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
